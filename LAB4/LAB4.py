1f652975b7d53a5d4275a1f1b3b8cb5100c2eeab
Md Hossain <hossai13@go.stockton.edu>
2024-03-15 21:12:51 -0400

    finished

--- LAB4/LAB4.py
+++ LAB4/LAB4.py
@@ -1,17 +1,57 @@
-# Finding the maximum number in an array:
-def max(a):
-    max = a[0]
+import math
+
+# Position Search: Finding the index of the maximum number in the array. 
+def max(array):
+    max = array[0]
     for i in range(1, len(a)):
-        if max < a[i]:
-            max = a[i]
+        if max < array[i]:
+            max = array[i]
     return max
 
-# Linear Search:
+# Linear Search: Finding a target element x in a sequece of integers.
+def linearSearch(array, x):
+    print()
+    i = 0
+    while i < len(array) and array[i] != x:
+        print("i = ", i, " |  a[i] = ", array[i])
+        i += 1
+    if i < len(array):
+        location = i
+        return location
+    else:
+        return -1
 
-# Binary Search:
+# Binary Search: Finding a target element x in an increasing ordered sequence of integers.
+def binarySearch(array, x):
+    i = 0
+    j = len(array) - 1
+    print()
+    while i < j:
+        m = math.floor((i + j) / 2)
+        print ("i = ", i, " |  j = ", j, " |  m = ", m, " |  a[i] = ", array[i], " |  a[m] = ", array[m])
+        if x > array[m]:
+            i = m + 1
+        else:
+            j = m
+    if x == array[i]:
+        location = i
+    else:
+        location = -1
+    return location
 
-# Bubble Sort:
 
+# Bubble Sort: Finding a target element x in an increasing ordered sequence of integers.
+def bubbleSort(array):
+    print()
+    n = len(array)
+    for i in range(n):
+        for j in range(n-1):
+            if array[j] > array[j+1]:
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+                print ("i = ", i, " |  j = ", j, " |  a[j] = ", array[j], " |  a[j+1] = ", array[j+1])
+    return array
 
 
 a = [3,4,5,1,6,8,7,9,2]
@@ -20,5 +60,5 @@ print("The location of the searched item with linear is ", linearSearch(a, 6))
 print("The location of the searched item is ", binarySearch(a, 5))
 
 a = [3, 4, 5, 1, 6, 8, 7, 9, 2]
-print("original array is ", a)
+print("\noriginal array is ", a)
 print("sorted array using bubble sort is ", bubbleSort(a))
